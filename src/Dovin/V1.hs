173f3f64315ed3de94ce0b7c37a07c8ec6389d4f
Xavier Shay <contact@xaviershay.com>
2019-03-09 14:08:30 -0800

    Remove unused run functions, demote fork to V1

--- src/Dovin/V1.hs
+++ src/Dovin/V1.hs
@@ -6,6 +6,7 @@ module Dovin.V1
   , module Dovin.Formatting
   , module Dovin.Helpers
   , module Dovin.Types
+  , fork
   , validate
   , validateLife
   , withLocation
@@ -23,7 +24,9 @@ import Dovin.Helpers
 import Dovin.Monad
 import Dovin.Types
 
+import Control.Monad (forM_)
 import Control.Monad.Reader (local)
+import Control.Monad.State (get, put)
 import Control.Lens (set, view)
 
 -- | Validate that a card matches a matcher.
@@ -67,3 +70,11 @@ trigger targetName = do
 
   return ()
 
+fork :: [GameMonad ()] -> GameMonad ()
+fork options = do
+  b <- get
+  let cs = view currentStep b
+
+  forM_ options $ \m -> do
+    m
+    put $ set currentStep cs b
