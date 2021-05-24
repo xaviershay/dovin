4b27bd5face16f3142fb3290df750b78d2462888
Xavier Shay <xavier@rhnh.net>
2021-05-24 14:59:49 +1000

    Upgrade test/Move.hs to V3

--- test/Move.hs
+++ test/Move.hs
@@ -1,82 +1,82 @@
 module Move where
 
-import TestPrelude
+import TestPrelude.V3
 
 test_Move = testGroup "move/moveTo"
   [ prove "moves card from one location to another" $ do
-      withLocation (Active, Hand) $ addLand "Forest"
+      withLocation Hand $ addLand "Forest"
       move (Active, Hand) (Active, Exile) "Forest"
 
-      validate "Forest" $ matchLocation (Active, Exile)
+      validate (matchZone Exile) "Forest"
   , prove "exiles jumpstart'ed cards" $ do
-      withLocation (Active, Stack) $ addInstant "Chemister's Insight"
+      withLocation Stack $ addInstant "Chemister's Insight"
       gainAttribute exileWhenLeaveStack "Chemister's Insight"
 
       move (Active, Stack) (Active, Graveyard) "Chemister's Insight"
 
-      validate "Chemister's Insight" $
-           matchLocation (Active, Exile)
+      validate (
+           matchZone Exile
         <> missingAttribute exileWhenLeaveStack
+        ) "Chemister's Insight"
   , prove "adds summoned attribute when moving to play" $ do
-      withLocation (Active, Hand) $ addLand "Forest"
+      withLocation Hand $ addLand "Forest"
       move (Active, Hand) (Active, Play) "Forest"
 
-      validate "Forest" $ matchAttribute summoned
+      validate (matchAttribute summoned) "Forest"
   , prove "removes gained attribute when leaving play" $ do
-      withLocation (Active, Play) $ addLand "Forest"
+      withLocation Play $ addLand "Forest"
 
       gainAttribute "bogus" "Forest"
 
       move (Active, Play) (Active, Hand) "Forest"
 
-      validate "Forest" $ missingAttribute "bogus"
+      validate (missingAttribute "bogus") "Forest"
   , prove "adds +1/+1 to when undying" $ do
-      withLocation (Active, Play)
+      withLocation Play
         $ withAttribute undying
         $ addCreature (1, 1) "Zombie"
 
       moveTo Graveyard "Zombie"
 
-      validate "Zombie" $
-        matchLocation (Active, Play) <> matchPlusOneCounters 1
+      validate (matchZone Play <> matchPlusOneCounters 1) "Zombie"
   , prove "moves to graveyard when undying and has +1/+1 counters" $ do
-      withLocation (Active, Play)
+      withLocation Play
         $ withAttribute undying
         $ withPlusOneCounters 3
         $ addCreature (1, 1) "Zombie"
 
       moveTo Graveyard "Zombie"
 
-      validate "Zombie" $
-        matchLocation (Active, Graveyard) <> matchPlusOneCounters 0
+      validate (matchZone Graveyard <> matchPlusOneCounters 0) "Zombie"
   , prove "removes damage when leaving play" $ do
-      withLocation (Active, Play) $ addCreature (1, 1) "Zombie"
+      withLocation Play $ addCreature (1, 1) "Zombie"
       modifyCardDeprecated "Zombie" cardDamage (const 1)
 
       moveTo Graveyard "Zombie"
 
-      validate "Zombie" $ matchDamage 0
+      validate (matchDamage 0) "Zombie"
   , prove "moves card to graveyard of same owner" $ do
-      withLocation (Opponent, Hand) $ addLand "Forest"
-      moveTo Graveyard "Forest"
-      validate "Forest" $ matchLocation (Opponent, Graveyard)
+      as Opponent $ do
+        withLocation Hand $ addLand "Forest"
+        moveTo Graveyard "Forest"
+        validate (matchController Opponent <> matchZone Graveyard) "Forest"
   , prove "can change controller of token" $ do
-      withLocation (Active, Play)
+      withLocation Play
         $ withAttribute token
         $ addArtifact "Treasure"
 
       move (Active, Play) (Opponent, Play) "Treasure"
-      validate "Treasure" $ matchLocation (Opponent, Play)
+      validate (matchZone Play) "Treasure"
   , refute
       "cannot move to stack"
       "cannot move directly to stack" $ do
-        withLocation (Active, Hand) $ addInstant "Shock"
+        withLocation Hand $ addInstant "Shock"
 
         move (Active, Hand) (Active, Stack) "Shock"
   , refute
       "cannot move to deck"
       "cannot move directly to deck" $ do
-        withLocation (Active, Graveyard) $ addInstant "Shock"
+        withLocation Graveyard $ addInstant "Shock"
 
         move (Active, Graveyard) (Active, Deck) "Shock"
   , refute
@@ -86,13 +86,13 @@ test_Move = testGroup "move/moveTo"
   , refute
       "cannot move to same location"
       "cannot move to same location" $ do
-        withLocation (Active, Hand) $ addInstant "Shock"
+        withLocation Hand $ addInstant "Shock"
 
         move (Active, Hand) (Active, Hand) "Shock"
   , refute
       "cannot move token from non-play location"
       "cannot move token from non-play location" $ do
-        withLocation (Active, Graveyard)
+        withLocation Graveyard
           $ withAttribute token
           $ addArtifact "Treasure"
 
@@ -100,7 +100,7 @@ test_Move = testGroup "move/moveTo"
   , refute
       "cannot move copy from non-stack location"
       "cannot move copy from non-stack location" $ do
-        withLocation (Active, Hand) $ addInstant "Shock"
+        withLocation Hand $ addInstant "Shock"
 
         cast "" "Shock"
         copySpell "Shock 1" "Shock" >> resolveTop
